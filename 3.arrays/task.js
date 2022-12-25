c1dcad41cf54f6c67dcdae575d7015e8affb3916
Igor Khomenko <xomigrek@gmail.com>
2022-12-25 22:43:11 +0300

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length===arr2.length && arr1.every((element, n) => element===arr2[n]);
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
+  return resultArr;
 }
