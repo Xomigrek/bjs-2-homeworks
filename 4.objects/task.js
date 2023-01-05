5489acc8f7b44cc5f6faee20c813b699281fd18e
Igor Khomenko <xomigrek@gmail.com>
2023-01-05 15:24:58 +0300

    Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,32 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+  this.name = name;
+  this.gender = gender;
+  this.age =age;
+  }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
-
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark)
+  };
+}
+Student.prototype.addMarks = function (...mark){
+  this.marks = [];
+  for (let m of mark) {
+    this.marks.push(m);
+  }
+}
+Student.prototype.getAverage = function () {
+  let sum = 0
+  this.marks.forEach(item => (sum += item));
+  return sum/this.marks.length;
+}
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
